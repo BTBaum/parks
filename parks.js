5cea34d78e6c25604c99d76c121c73882ea6af2b
Brandon <railsdev14@gmail.com>
2016-01-24 12:39:11 -0500

    Added parks helper to fetch all park data

--- parks.js
+++ parks.js
@@ -1,25 +1,11 @@
 Parks = new Mongo.Collection("parks");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    parks: function () {
+      return Parks.find().fetch();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
