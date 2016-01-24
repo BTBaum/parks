f1ba960c55a3e382006a7aae24d248528f0dd8d8
Brandon <railsdev14@gmail.com>
2016-01-24 14:20:50 -0500

    Added seleced class when park is clicked

--- parks.js
+++ parks.js
@@ -3,9 +3,24 @@ Parks = new Mongo.Collection("parks");
 if (Meteor.isClient) {
 
   Template.body.helpers({
-    parks: function () {
-      return Parks.find().fetch();
+    park: function () {
+      return Parks.find()
+    },
+    'selectedClass': function() {
+      var selectedId = "a" + this._id.valueOf();
+      var selectedPark = Session.get('selectedPark');
+      if(selectedId == selectedPark) {
+        return "selected"
+      }
     }
   });
 
+  Template.body.events({
+    'click .park': function() {
+      var parkId = "a" + this._id.valueOf();
+      Session.set('selectedPark', parkId);
+      console.log(parkId);
+      // var selectedPark = Session.get('selectedPark');
+    }
+  });
 }
