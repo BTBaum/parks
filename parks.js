5a68b56c8f5f9c5fd5ce2a151c01cbcce261424f
Brandon <railsdev14@gmail.com>
2016-01-25 00:29:59 -0500

    Added nearBy helper

--- parks.js
+++ parks.js
@@ -3,13 +3,20 @@ Parks = new Mongo.Collection("parks");
 if (Meteor.isClient) {
 
   Template.body.helpers({
-    park: function () {
+    park: function() {
+      var selectedPark = Session.get('selectedPark');
       return Parks.find()
+    },
+    nearBy: function(limit) {
+      var limit = limit || 5;
+      var selectedParkPos = Session.get('selectedParkPos');
+      var nearByParks = Parks.find({"pos": {$near:selectedParkPos}}, {sort: {count:-1}, limit: 5});
+      return nearByParks;
     },
     'selectedClass': function() {
       var selectedId = "a" + this._id.valueOf();
       var selectedPark = Session.get('selectedPark');
-      if(selectedId == selectedPark) {
+      if(selectedId == "a" + selectedPark) {
         return "selected"
       }
     }
@@ -17,10 +24,14 @@ if (Meteor.isClient) {
 
   Template.body.events({
     'click .park': function() {
-      var parkId = "a" + this._id.valueOf();
+      var parkId = this._id.valueOf();
+      var pos = this.pos;
       Session.set('selectedPark', parkId);
-      console.log(parkId);
-      // var selectedPark = Session.get('selectedPark');
+      Session.set('selectedParkPos', pos);
     }
   });
+}
+
+if (Meteor.isServer) {
+
 }
